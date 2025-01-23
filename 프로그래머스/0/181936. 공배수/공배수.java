b29584fbc21cae62cbcfcb9670b6b3fe6cede4bb
daeun623 <124323003+daeun623@users.noreply.github.com>
2025-01-23 12:11:25 +0900

    [level 0] Title: 공배수, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.java
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(int number, int n, int m) {
-        if(m%n==0) {
-            n=1;
-        }else if(n%m==0) {
-            m=1;
-        }
-        int answer = (number%(n*m)==0) ? 1 : 0;
+        int answer = (number%n==0 && number%m==0) ? 1 : 0;
         return answer;
     }
 }
